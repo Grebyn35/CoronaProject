7238ed65974a322d0aa3952b5def4bc2c3ebd22b
Grebyn35 <elliot.heathnyberg@gmail.com>
2020-09-10 15:18:46 +0200

    random commit

--- Inlämning/src/App.js
+++ Inlämning/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Cards from "./components/Cards/Cards"
 import CountryPicker from "./components/CountryPicker/CountryPicker"
-import Chart from "./components/Chart/Chart"
+import Chart from "./components/Chart"
 import styles from "./App.module.css"
 import {fetchData} from "./api/index"
 
